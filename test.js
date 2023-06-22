d44764ce0f7ad98fd8197a26c919c60175601554
Adxzan <133544171+Adxzan@users.noreply.github.com>
2023-06-22 22:52:37 +0200

    Signed-off-by: Adxzan <133544171+Adxzan@users.noreply.github.com>

--- test.js
+++ test.js
@@ -1,29 +1,40 @@
-function toggleMenu() {
-  var menuToggleCheckbox = document.getElementById("menu-toggle-checkbox");
-  var bmenuContent = document.getElementById("burgermenu","bmenu-content");
-
-  if (menuToggleCheckbox.checked) {
-    bmenuContent.style.display = "flex";
-  } else {
-    bmenuContent.style.display = "none";
-  }
-  if (menuToggleCheckbox.unchecked){
-    bmenuContent.hidden = false;
-  } else {
-    bmenuContent.hidden = true;
-  }
-
-  
-}
-function toggleDropdown() {
+document.addEventListener("DOMContentLoaded", function () {
   var burgerMenu = document.getElementById("burgermenu");
-  var bmenuContent = document.getElementById("bmenu-content");
+  burgerMenu.style.display = "flex";
+  burgerMenu.addEventListener("click", toggleDropdown);
+  burgerMenu.addEventListener("touchend", toggleDropdown);
+});
 
-  if (bmenuContent.style.display === "none") {
-    bmenuContent.style.display = "flex";
-    burgerMenu.classList.add("active");
+function toggleDropdown() {
+  var bmenuContent = document.getElementById("bmenu-content");
+  var isVisible = window.getComputedStyle(bmenuContent).display !== "none";
+
+  bmenuContent.style.display = isVisible ? "none" : "flex";
+}
+
+function toggleMenu() {
+  var menuToggleRadio = document.getElementById("menu-toggle-radio");
+  //var menuToggleRadio1 = document.getElementById("menu-toggle-radio1");
+  //var menuToggleRadio2 = document.getElementById("menu-toggle-radio2");
+  var bmenuContent = document.getElementById("bmenu-content");
+  var burgerMenu = document.getElementById("burgermenu");
+  //var header = document.getElementById("header");
+  //var hdparent = document.getElementById("hdparent");
+
+  var anyChecked = Array.from(menuToggleRadio).some(function (radio) {
+    return radio.checked;
+  });
+
+  if (anyChecked) {
+    bmenuContent.style.display = "none";
+    burgerMenu.style.display = "none";
   } else {
     bmenuContent.style.display = "none";
-    burgerMenu.classList.remove("active");
+    burgerMenu.style.display = "flex";
   }
+  //if ((menuToggleRadio1 = radio.checked)) {
+  //  hdparent.style.display = "flex";
+  //  bmenuContent.style.display = "none";
+  //  burgerMenu.style.display = "none";
+  //}
 }
